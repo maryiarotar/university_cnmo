8eaca7057917e1b5e0751075fe3b4e52dcf827b0
Maryia Rotar <rotarmaryia@gmail.com>
2023-02-18 15:06:28 +0200

    method_dichotomy

--- src/functions.rs
+++ src/functions.rs
@@ -1,14 +1,42 @@
 use std::io;
 use std::io::Write;
+use std::time::{Instant};
+//use num_traits::AsPrimitive;
 
 /*dichotomy method*/
-pub(crate) fn dichotomy(start :f64, end :f64, precision :f64) -> f64 {
+pub(crate) fn dichotomy(a :f64, b :f64, precision :f64) -> (f64, i8) {
+    let mut start = a;
+    let mut end = b;
+    let mut iteration :i8 = 0;
 
-    let middle =
+    loop {
+        let middle = (start + end) / 2.0;
+        let func_a = function_x(start);
+        let func_c = function_x(middle);
+        let func_b = function_x(end);
+        iteration += 1;
 
+        // f(a)*f(c)>0 => there is no root and we work with [c, b]. And vice versa.
+        if (func_a * func_c) > 0.0 {
+            start = middle;
+        }
+        if (func_c * func_b) > 0.0 {
+            end = middle;
+        }
+
+        if func_c == 0.0 { return (middle, iteration); } //if c==0, it's the root!
+        if (end - start) <= precision {
+            return ((start + end) / 2.0, iteration);
+        }
+    }
 }
 
 
+/* f(x) = 𝑥^2 𝑐𝑜𝑠2𝑥 + 1 */
+fn function_x(x :f64) -> f64 {
+    let func_x :f64 = x.sqrt() * (2.0*x).cos() + 1.0;
+    func_x
+}
 
 /*function to enter a number*/
 pub(crate) fn in_and_parse_number() -> f64 {
@@ -30,4 +58,12 @@ pub(crate) fn in_and_parse_number() -> f64 {
         };
         return temp;
     }
+}
+
+pub(crate) fn how_long_to_count(f :fn(f64, f64, f64) -> (f64, i8),
+                     a :f64, b :f64, pr :f64) {
+    let start = Instant::now();
+    f(a, b , pr);
+    let end = start.elapsed();
+    println!("performance = {} seconds ({} nanosecs) ", end.as_secs_f64(), end.as_nanos());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+extern crate approx;
+extern crate num_traits;
+
 mod functions;
-use functions::{in_and_parse_number, dichotomy};
+use functions::{in_and_parse_number, dichotomy, how_long_to_count};
 
 fn main() {
 
@@ -9,11 +12,13 @@ fn main() {
 
     println!("a = {a}, b = {b}, e = {e}");
 
-    //dichotomy(a, b, e );
+    let d :(f64, i8) = dichotomy(a, b, e );
+    println!("метод дихотомии. Корень: {}, итераций: {}", d.0, d.1);
 
 
 
 
+    how_long_to_count(dichotomy, a, b, e);
 
 
 
